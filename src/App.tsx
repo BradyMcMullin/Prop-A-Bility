6e9b1c5626083b5f87bf0edd6f7984ac242b928c
Brady McMullin <bradynmcmullin@gmail.com>
2026-02-04 20:27:41 -0700

    fixed type error

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { supabase } from './lib/supabase';
-import { Session } from '@supabase/supabase-js';
+import type { Session } from '@supabase/supabase-js';
 import Auth from './components/Auth';
 import './App.css';
 
